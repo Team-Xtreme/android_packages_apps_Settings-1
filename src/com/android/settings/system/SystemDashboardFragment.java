dbf84c970f3d0fd2ff0e718cfe972fcc3a828cb3
rpingat <pingatravi@gmail.com>
2020-02-11 13:40:17 +0530

    Revert "Settings: Add Updater"
    This reverts commit 31ca3bdcaf0df8874feaeb03e5eb500d28c3667b.

--- src/com/android/settings/system/SystemDashboardFragment.java
+++ src/com/android/settings/system/SystemDashboardFragment.java
@@ -31,12 +31,9 @@ import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
-import com.android.settings.deviceinfo.aboutphone.OTAPreferenceController;
-import com.android.settingslib.core.AbstractPreferenceController;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 
 @SearchIndexable
 public class SystemDashboardFragment extends DashboardFragment {
@@ -100,16 +97,6 @@ public class SystemDashboardFragment extends DashboardFragment {
             }
         }
         return visibleCount;
-    }
-
-    protected List<AbstractPreferenceController> getPreferenceControllers(Context context) {
-        return buildPreferenceControllers(context);
-    }
-
-    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
-        final List<AbstractPreferenceController> controllers = new ArrayList<>();
-        controllers.add(new OTAPreferenceController(context));
-        return controllers;
     }
 
     /**
